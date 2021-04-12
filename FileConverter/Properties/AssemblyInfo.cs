86fb9aee3e5d5e96ab1aac75e8044b9761d1fad9
lukashecke <57808594+lukashecke@users.noreply.github.com>
2021-04-12 09:02:10 +0200

    assemblyinfo for next version

--- FileConverter/Properties/AssemblyInfo.cs
+++ FileConverter/Properties/AssemblyInfo.cs
@@ -7,11 +7,11 @@ using System.Windows;
 // Allgemeine Informationen über eine Assembly werden über die folgenden
 // Attribute gesteuert. Ändern Sie diese Attributwerte, um die Informationen zu ändern,
 // die einer Assembly zugeordnet sind.
-[assembly: AssemblyTitle("FileConverter")]
+[assembly: AssemblyTitle("File Converter")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Lukas Hecke")]
-[assembly: AssemblyProduct("FileConverter")]
+[assembly: AssemblyProduct("File Converter")]
 [assembly: AssemblyCopyright("Copyright ©  2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -51,5 +51,5 @@ using System.Windows;
 // Sie können alle Werte angeben oder Standardwerte für die Build- und Revisionsnummern verwenden,
 // übernehmen, indem Sie "*" eingeben:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.1.0.0")]
+[assembly: AssemblyFileVersion("1.1.0.0")]
